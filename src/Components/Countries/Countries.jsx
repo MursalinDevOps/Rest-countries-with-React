ee3bc6defdaf39b266ca7b9342e6120aa7354290
MursalinDevOps <mursalin1768@gmail.com>
2024-10-18 15:39:11 +0600

    adding visited countries list on the UI functionality added

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -4,17 +4,38 @@ import Country from "../Country/Country";
 import './Countries.css'
 const Countries = () => {
     const [countries, setCountries] = useState([]);
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
+    const handleVisitedCountry = (country) => {
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+    }
+
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data => setCountries(data))
     },[])
     return (
-        <div style={{textAlign:'center'}}>
-           <h2>Countries: {countries.length}</h2>
+        
+        <div>
+           <h2  style={{textAlign:'center'}}>Countries: {countries.length}</h2>
+           <div style={{paddingLeft:'20px'}}>
+            <h2>
+            Visited Countries: {visitedCountries.length}
+            </h2>
+            <ul>
+                {
+                    visitedCountries.map(country => <li key={country.cca3}>{country.name.common} </li>)
+                }
+            </ul>
+           </div>
           <div className="country-container">
           {
-            countries.map(country => <Country key={country.cca3} country={country}></Country>)
+            countries.map(country => <Country 
+                key={country.cca3}
+                handleVisitedCountry={()=>handleVisitedCountry(country)}
+                country={country}></Country>)
            }
           </div>
         </div>
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
-function Country({country}) {
+function Country({country, handleVisitedCountry}) {
     const {flags,name, capital, population, area} = country;
     const [isVisited, setIsVisited] = useState(false);
+
+    
     const handleIsVisited = () => {
         setIsVisited(!isVisited);
     } 
+    // console.log(handleVisitedCountry)
     return (
         <div style={{border:'2px solid tomato', margin:'20px', padding:'20px',textAlign:'left', borderRadius:'10px'}}>
             <div>
@@ -15,7 +18,15 @@ function Country({country}) {
             <h3>Capital : {capital}</h3>
             <p>Population: {population}</p>
             <p>Area: {area}</p>
-            <button onClick={handleIsVisited} style={{width:'100%',height:'56px', backgroundColor:'#888',color:'black', border:'none', outline:'none', borderRadius:'10px',fontSize:'18px',fontWeight:'bold'}}>{isVisited ? 'Visited' : 'Visit'}</button>
+            <button onClick={handleIsVisited} style={{width:'100%',height:'56px', backgroundColor: isVisited ? '#fff' : '#888',color:'black', border:'none', outline:'none', borderRadius:'10px',fontSize:'18px',fontWeight:'bold'}}>{isVisited ? 'Visited' : 'Visit'}</button>
+            <br />
+            <hr />
+
+            <button onClick={handleVisitedCountry}
+            style={{width:'100%',height:'56px', backgroundColor:'tomato',border:'none', outline:'none', borderRadius:'10px',fontSize:'18px'}}>
+                Mark as Visited
+            </button>
+
         </div>
     );
 }
